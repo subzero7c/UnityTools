ffd193d6c1403eab3675c071b02272949e7d1fd5
subzero7c <604151294@qq.com>
2020-05-10 23:48:07 +0800

    feat:添加导入资源监测功能

---
+++ UnityTools/Assets/Editor/Process/ProcessBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ProcessBase : AssetPostprocessor
+{
+    // Start is called before the first frame update
+    public static bool isUseProcess = false;
+    /// <summary>
+    /// 处理导入模型
+    /// </summary>
+    /// <param name="input"></param>
+     void  OnPostprocessModel (GameObject input){
+         if (isUseProcess )
+         return;
+         ProcessModel.OnPostprocessModel(input);
+     }
+    /// <summary>
+    /// 处理导入图片
+    /// </summary>
+    /// <param name="input"></param>
+    void OnPostprocessTexture(GameObject input) {
+        if (isUseProcess)
+            return;
+    }
+    void OnPreprocessAnimation(GameObject input) {
+        if (isUseProcess)
+            return;
+
+
+    }
+    void OnPreprocessAudio(GameObject input) {
+        if (isUseProcess)
+            return;
+
+
+    }
+}
---
+++ UnityTools/Assets/Editor/Process/ProcessModel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public static class ProcessModel {
+
+    public static void OnPostprocessModel (GameObject input) {
+        // 设置导入模型的tag
+        //input.tag = "";
+        return;
+        // 取得导入模型的相关信息
+        ModelImporter importer = assetImporter as ModelImporter;
+
+        AssetDatabase.Refresh ();
+
+        // 从工程中将该模型读出来
+        GameObject tar = AssetDatabase.LoadAssetAtPath (importer.assetPath, typeof (GameObject)) as GameObject;
+
+        if (tar) {
+            GameObject obj = GameObject.Instantiate (tar);
+
+            Texture2D textureData = AssetDatabase.LoadAssetAtPath (("Assets/BundleResources/Texture/" + input.name + ".png"), typeof (Texture2D)) as Texture2D;
+
+            if (textureData) {
+                Material importMat = new Material (Shader.Find ("Mobile/Unlit (Supports Lightmap)"));
+                importMat.mainTexture = textureData;
+                AssetDatabase.CreateAsset (importMat, "Assets/BundleResources/Materials/" + input.name + ".mat");
+
+                Material[] materials = {
+                    AssetDatabase.LoadAssetAtPath (("Assets/BundleResources/Materials/" + input.name + ".mat"), typeof (Material)) as Material
+                };
+                obj.GetComponent<MeshRenderer> ().materials = materials;
+                // 将这个模型创建为Prefab
+                GameObject prefab = PrefabUtility.SaveAsPrefabAsset (obj, ("Assets/BundleResources/Prefabs/" + input.name + ".Prefab"));
+            }
+        }
+    }
+}
